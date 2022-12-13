5ddea01600287e924f0b935e2d81b661abeb1de6
Mustafa80baz <112826649+Mustafa80baz@users.noreply.github.com>
2022-12-13 20:02:32 +0300

    ubdated1

--- src/Tablet.java
+++ src/Tablet.java
@@ -1,7 +1,7 @@
 public class Tablet extends techEquipments{
     public Tablet(String brand, String model, String color, double price, int expirationYear, boolean isSecondHand) {
         super(brand, model, color, price, expirationYear, isSecondHand);
-    } //assigned to Mustafa
-    // neden olmuyor bu  yeniden başala artıkı
+    } //dest busi arzusu ile ölürsem dostlar
+    //Kuze eyleyin sunun anınla yare su
 
 }
