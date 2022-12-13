88d531409b3cd30518f9032dcc8630be8813964e
Zafer Mengeloglu <fer.istatistik@gmail.com>
2022-12-13 18:40:40 +0300

    created TechnoShop.java

---
+++ src/TechnoShop.java
@@ -0,0 +1,11 @@
+public class TechnoShop {
+
+    public static void main(String[] args) {
+
+        /*
+        We're gonna create objects which are in our store and
+        ready to be sold
+
+         */
+    }
+}
