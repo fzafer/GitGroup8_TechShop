d7045cab2fe49cd6df1f5c1c2a64c2ac5a2fa2a1
Zafer Mengeloglu <fer.istatistik@gmail.com>
2022-12-13 18:33:21 +0300

    created SmartWatch.java

---
+++ src/SmartWatch.java
@@ -0,0 +1,5 @@
+public class SmartWatch extends techEquipments{
+    public SmartWatch(String brand, String model, String color, double price, int expirationYear, boolean isSecondHand) {
+        super(brand, model, color, price, expirationYear, isSecondHand);
+    }
+}
