2c01445831ef017714f6b4780af952ffd1deb9d3
Zafer Mengeloglu <fer.istatistik@gmail.com>
2022-12-13 18:33:30 +0300

    created Tablet.java

---
+++ src/Tablet.java
@@ -0,0 +1,5 @@
+public class Tablet extends techEquipments{
+    public Tablet(String brand, String model, String color, double price, int expirationYear, boolean isSecondHand) {
+        super(brand, model, color, price, expirationYear, isSecondHand);
+    }
+}
