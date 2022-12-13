1ba063266964fb41c3d7b13bcb5dd071e39e560b
Zafer Mengeloglu <fer.istatistik@gmail.com>
2022-12-13 18:54:42 +0300

    created Television.java

---
+++ src/Television.java
@@ -0,0 +1,5 @@
+public class Television extends techEquipments{
+    public Television(String brand, String model, String color, double price, int expirationYear, boolean isSecondHand) {
+        super(brand, model, color, price, expirationYear, isSecondHand);
+    }
+}
