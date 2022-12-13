37a1c6936cb7edbc70a08bc5b7e60d5dea40187c
Zafer Mengeloglu <fer.istatistik@gmail.com>
2022-12-13 19:55:41 +0300

    assignment to zülal

---
+++ src/Projector.java
@@ -0,0 +1,6 @@
+public class Projector extends techEquipments{
+    public Projector(String brand, String model, String color, double price, int expirationYear, boolean isSecondHand) {
+        super(brand, model, color, price, expirationYear, isSecondHand);
+    }
+    //assigned to Zülal
+}
