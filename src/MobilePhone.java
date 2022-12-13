cd49ed27ed88c7bdb0666a718279bed1a468d220
Zafer Mengeloglu <fer.istatistik@gmail.com>
2022-12-13 18:32:54 +0300

    created MobilePhone.java

---
+++ src/MobilePhone.java
@@ -0,0 +1,5 @@
+public class MobilePhone extends techEquipments{
+    public MobilePhone(String brand, String model, String color, double price, int expirationYear, boolean isSecondHand) {
+        super(brand, model, color, price, expirationYear, isSecondHand);
+    }
+}
