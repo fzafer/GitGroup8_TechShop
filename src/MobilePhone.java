32fda5b400b57a24dab9858fb96679981c49baf4
Zafer Mengeloglu <fer.istatistik@gmail.com>
2022-12-14 16:56:09 +0300

    filled up the codes (zafer)

--- src/MobilePhone.java
+++ src/MobilePhone.java
@@ -1,6 +1,43 @@
 public class MobilePhone extends techEquipments{
     public MobilePhone(String brand, String model, String color, double price, int expirationYear, boolean isSecondHand) {
         super(brand, model, color, price, expirationYear, isSecondHand);
+
     }
     //assigned to Zafer
+
+    public String screenSize;
+    public String OperatingSystem;
+    public int storage;
+    public int RAM;
+    public int screenResolution;
+    public int maxCamResolution;
+
+
+    public boolean hasRemovableBattery;
+    public boolean hasMemoryCardSupport;
+    public boolean hasWirelessCharging;
+
+    public void call(long phoneNumber) {
+        System.out.println(getBrand() + " " + getModel() + " is calling " + phoneNumber);
+    }
+
+    public void text(long phoneNumber) {
+        System.out.println(getBrand() + " " + getModel() + " is texting " + phoneNumber);
+    }
+
+    @Override
+    public String toString() {
+        return "MobilePhone{" +
+                "screenSize='" + screenSize + '\'' +
+                ", OperatingSystem='" + OperatingSystem + '\'' +
+                ", storage=" + storage +
+                ", RAM=" + RAM +
+                ", screenResolution=" + screenResolution +
+                ", maxCamResolution=" + maxCamResolution +
+                ", hasRemovableBattery=" + hasRemovableBattery +
+                ", hasMemoryCardSupport=" + hasMemoryCardSupport +
+                ", hasWirelessCharging=" + hasWirelessCharging +
+                '}';
+    }
 }
+
