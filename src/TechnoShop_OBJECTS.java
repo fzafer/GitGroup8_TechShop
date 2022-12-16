3a99e151bded7b49c9332b5bc89ec2035e9773f5
ugmail <unenmail@gmail.com>
2022-12-16 19:27:30 +0300

    smartwatch object created

--- src/TechnoShop_OBJECTS.java
+++ src/TechnoShop_OBJECTS.java
@@ -11,6 +11,7 @@ public class TechnoShop_OBJECTS {
         Television_Mesut televisionMesut1 = new Television_Mesut("Arçelik","AC101", "Black",10000,2020,false );
         Tablet_Mustafa tablet1=new Tablet_Mustafa("Samsun","Note7","black",2300,2023,false);
         EarPods_Vahap earPods1=new EarPods_Vahap("Samsung","w101","white",350,2021,false);
+        SmartWatch_Serkan smartWatch1 = new SmartWatch_Serkan("Apple","SmartWatch","White",500.0,2030,false);
 
 
 
